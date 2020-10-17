9140428b6004d14975cd75d4b92da2565499c67a
Daniel Garcia <business_dan@protonmail.com>
2020-10-17 15:44:02 -0500

    works section text added

--- src/components/Works.js
+++ src/components/Works.js
@@ -4,7 +4,20 @@ import "./style/works.css";
 function Works() {
   return (
     <div className="work-container">
-      <p>Works</p>
+      <div className="work-info">
+        <div className="work-text">
+          <div className="work-title">- Portfolio All Creative Works.</div>
+          <div className="work-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet
+            pharetra, amet id sagittis, nisl in augue. Sed blandit egestas diam
+            in ornare.
+          </div>
+        </div>
+        <div className="work-image">
+          <img src="/media/red.svg" alt="red" />
+        </div>
+      </div>
+      <div className="projects-container"></div>
     </div>
   );
 }

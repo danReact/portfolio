3e0f6f6532777577f9bfe8b3360d6f6454b4a0ea
Daniel Garcia <business_dan@protonmail.com>
2020-10-17 14:59:31 -0500

    introduction structure finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Introduction from "./Introduction";
+import Works from "./Works";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Introduction />
+      <Works />
     </div>
   );
 }
---
+++ src/components/Works.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style/works.css";
+
+function Works() {
+  return (
+    <div className="work-container">
+      <p>Works</p>
+    </div>
+  );
+}
+
+export default Works;

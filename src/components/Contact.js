5f2ce1e9d897ab12bde5d9f0e04681f124aeb796
Daniel Garcia <business_dan@protonmail.com>
2020-10-20 14:42:30 -0500

    email sender activated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,30 @@
 import React from "react";
+import emailjs from "emailjs-com";
 import "./style/contact.css";
 
 function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "first_template",
+        e.target,
+        "user_sz4hfUdyeYVh5j2olKNjN"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  }
+
   return (
     <div className="contact-container" id="contact-container">
       <div className="contact-info">
@@ -14,26 +37,32 @@ function Contact() {
         <div className="contact-description">
           My inbox is always open. Whether you have a question, project or just
           want to say hi, I'll try my best to get back to you!
-          <p>daniel@dgtdan.com</p>
+          <p>daniel@gtdan.com</p>
         </div>
       </div>
       <div className="contact-form">
-        <div className="form-section">
-          <div className="form-label">What´s your name?</div>
-          <input type="text" />
-        </div>
-        <div className="form-section">
-          <div className="form-label">Your fancy email</div>
-          <input type="email" />
-        </div>
-        <div className="form-section">
-          <div className="form-label send-mail">
-            Tell me about your project
-            <i className="fas fa-paperclip"></i>
-            <i className="fas fa-arrow-right"></i>
+        <form onSubmit={sendEmail}>
+          <div className="form-section">
+            <div className="form-label">What´s your name?</div>
+            <input type="text" name="name" />
           </div>
-          <textarea id="styled" rows="1" />
-        </div>
+          <div className="form-section">
+            <div className="form-label">Your fancy email</div>
+            <input type="email" name="email" />
+          </div>
+          <div className="form-section">
+            <div className="form-label send-mail">
+              Tell me about your project
+              {/* <button>
+                <i className="fas fa-paperclip"></i>
+              </button> */}
+              <button type="submit">
+                <i className="fas fa-arrow-right"></i>
+              </button>
+            </div>
+            <textarea id="styled" rows="1" name="message" />
+          </div>
+        </form>
       </div>
     </div>
   );
